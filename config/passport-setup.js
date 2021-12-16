8622011ed9425ca8bb43441ba17c584914969034
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-12-16 01:37:17 -0500

    google keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,11 @@
 const passport = require("passport")
 const GoogleStrat = require("passport-google-oauth20")
-
+const keys = require("./keys")
 passport.use(
     new GoogleStrat({
     //options for strategy
-
+    clientID:keys.google.clientId,
+    clientSecret:keys.google.clientSecret
     }),()=>{
         //passport callback
     }
