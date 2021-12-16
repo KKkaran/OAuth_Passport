f16732994f147a69fcf7ed7b4896fc3a72ff521f
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-12-16 01:41:55 -0500

    dsdsd

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google:{
-        clientId: "438219019316-fcjfsl9938efsipndftu5t61lv5uon87.apps.googleusercontent.com",
-        clientSecret: "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"
-    }
-}
